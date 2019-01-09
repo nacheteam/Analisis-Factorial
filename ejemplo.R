44bceb3cc9db83187f0653ea6aeb32385a7ec388
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-01-09 18:23:39 +0100

    Ejemplo de CFA

--- ejemplo.R
+++ ejemplo.R
@@ -3,6 +3,11 @@ cov_matrix <- Harman74.cor$cov
 library(psych)
 library(GPArotation)
 library(cfa)
+# Usado para los datos en CFA y la sintaxis lavaan
+library(MBESS)
+library(lavaan)
+
+##                                 EFA                                       ##
 
 ###############################################################################
 ##                               With rotation                               ##
@@ -26,7 +31,7 @@ mle_rotated <- factanal(covmat = cov_matrix, factors = 4)
 # Weighted least squares
 wls_nonrotated <- fa(cov_matrix,4 , rotate = "none", fm="wls")
 # Principal Axis
-pa_nonrotates <- fa(cov_matrix, 4, rotate = "none", fm="pa")
+pa_nonrotated <- fa(cov_matrix, 4, rotate = "none", fm="pa")
 # Minres
 minres_nonrotated <- factanal(factors=4,covmat=cov_matrix,rotation="none")
 # Maximum likelihood
@@ -34,24 +39,36 @@ mle_nonrotated <- fa(cov_matrix, 4, rotate = "none", fm="mle")
 # Unweighted least squares
 uls_nonrotated <- fa(cov_matrix, 4, rotate = "none", fm="uls")
 
-cat("\n\n\n          ----------------\n          | CON ROTACI脫N |\n          ----------------\n\n\n")
-cat("Resultados de principal axis con rotaci贸n:\n\n")
+cat("\n\n\n          ----------------\n          | CON ROTACION |\n          ----------------\n\n\n")
+cat("Resultados de principal axis con rotacion:\n\n")
 print(pa_rotated)
-cat("\n\nResultados de unweighted least squares con rotaci贸n:\n\n")
+cat("\n\nResultados de unweighted least squares con rotacion:\n\n")
 print(uls_rotated)
-cat("\n\nResultados de weighted least squares con rotaci贸n:\n\n")
+cat("\n\nResultados de weighted least squares con rotacion:\n\n")
 print(wls_rotated)
-cat("\n\nResultados de maximum likelihood con rotaci贸n:\n\n")
+cat("\n\nResultados de maximum likelihood con rotacion:\n\n")
 print(mle_rotated)
 
-cat("\n\n\n          ----------------\n          | SIN ROTACI脫N |\n          ----------------\n\n\n")
-cat("Resultados de weighted least squares sin rotaci贸n:\n\n")
+cat("\n\n\n          ----------------\n          | SIN ROTACION |\n          ----------------\n\n\n")
+cat("Resultados de weighted least squares sin rotacion:\n\n")
 print(wls_nonrotated)
-cat("Resultados de principal axis sin rotaci贸n:\n\n")
+cat("Resultados de principal axis sin rotacion:\n\n")
 print(pa_nonrotated)
-cat("Resultados de minres sin rotaci贸n:\n\n")
+cat("Resultados de minres sin rotacion:\n\n")
 print(minres_nonrotated)
-cat("Resultados de maximum likelihood sin rotaci贸n:\n\n")
+cat("Resultados de maximum likelihood sin rotacion:\n\n")
 print(mle_nonrotated)
-cat("Resultados de unweighted least squares sin rotaci贸n:\n\n")
+cat("Resultados de unweighted least squares sin rotacion:\n\n")
 print(uls_nonrotated)
+
+##                                 CFA                                       ##
+# Hacemos nuestra hip髏esis del modelo
+HS.model <- ' visual  =~ x1 + x2 + x3      
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+
+# Comprobamos la hip髏esis
+fit <- cfa(HS.model, data=HolzingerSwineford1939)
+
+# Comprobamos los datos sobre el ajuste
+summary(fit, fit.measures=TRUE)
